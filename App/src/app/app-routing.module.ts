ad2d9fafefa87e8532130256bd9b940624d398cc
Jithender <Jithender.Konduri@trigyn.com>
2021-03-07 12:32:09 +0530

    Ionic basic view uploading

---
+++ App/src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./home/home.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./screens/login/login.module').then( m => m.LoginPageModule)
+   }
+  // {
+  //   path: 'home',
+  //   loadChildren: () => import('./screens/home/home.module').then( m => m.HomePageModule)
+  // }
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
