552cf4b4c501d6bd4dec58ecfa7dd5712e2baae4
Jithender <Jithender.Konduri@trigyn.com>
2021-04-02 17:53:10 +0530

    Brand admin is completed

--- Api/server.js
+++ Api/server.js
@@ -6,6 +6,7 @@ const dbConfig = require("./app/config/db.config");
 const path = require('path');
 const app = express();
 const apiErrorHandler = require('./app/error/api-error-handler');
+var bcrypt = require("bcryptjs");
 // var corsOptions = {
 //   origin: "http://localhost:8100"
 // };
@@ -31,8 +32,8 @@ app.use((req, res, next) => {
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
   if (req.method === 'OPTIONS') {
-      res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-      return res.status(200).json({});
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
   }
   next();
 });
@@ -49,9 +50,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const db = require("./app/models");
 const Role = db.role;
 const Brand = db.brand;
+const User = db.user;
 
 db.mongoose
-//  .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
+  //  .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
   .connect(`mongodb+srv://jithender:admin@123@cluster0.ve2xe.mongodb.net/agrisoft`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -74,7 +76,7 @@ app.use(function (req, res, next) {
 });
 // routes
 require("./app/routes/auth.routes")(app);
-// require("./app/routes/user.routes")(app);
+require("./app/routes/user.routes")(app);
 require("./app/routes/brands.routes")(app);
 require("./app/routes/product.routes")(app);
 require("./app/routes/warehouse.routes")(app);
@@ -125,6 +127,15 @@ function initial() {
 
         console.log("added 'admin' to roles collection");
       });
+      new Role({
+        name: "brand-admin"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'brand admin' to roles collection");
+      });
     }
   });
   Brand.estimatedDocumentCount((err, count) => {
@@ -161,6 +172,38 @@ function initial() {
 
         console.log("added 'Malabar Milk' to Brand collection");
       });
+    }
+  });
+  User.estimatedDocumentCount((err, count) => {
+    if (!err) {
+      let newPassword = 'admin';
+      Role.findOne({
+        name: 'admin'
+      })
+        .exec((err, role) => {
+          if (role) {
+            console.log(role);
+            if (role.length === 0) {
+              bcrypt.genSalt(10, function (err, salt) {
+                // Call error-handling middleware:
+                if (err) { return res.send({ error: true, message: err }); }
+                bcrypt.hash(newPassword, salt, function (err, hash) {
+                  new User({
+                    name: "admin",
+                    username: "admin",
+                    email: "admin@yopmail.com",
+                    password: hash,
+                    roles: role._id
+                  }).save(err => {
+                    if (err) {
+                      console.log("error", err);
+                    }
+                  })
+                });
+              })
+            }
+          }
+        })
     }
   });
 }
