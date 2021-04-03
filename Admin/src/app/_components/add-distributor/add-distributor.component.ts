9a011e560bcbe041a6456588ab29e9a498842972
kapil.pedugula@rampgroup.com <kapil.pedugula@rampgroup.com>
2021-04-03 19:40:51 +0530

    new changes

--- Admin/src/app/_components/add-distributor/add-distributor.component.ts
+++ Admin/src/app/_components/add-distributor/add-distributor.component.ts
@@ -5,6 +5,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { AlertService } from 'src/app/_service/alert.service';
 import { first } from 'rxjs/operators';
 import { BrandService, BrandsType } from 'src/app/_service/brand.service';
+import { CookieService } from 'ngx-cookie-service';
 
 class productdata{
   productId:string;
@@ -21,12 +22,12 @@ export class AddDistributorComponent implements OnInit {
   loading = false;
   submitted = false;
   returnUrl: string;
-
-
+  id: string;
+  formType: string = "Add New";
   routeList: any[];
   DropPoint: any[];
   seletedBrand: BrandsType;
-  brandProducts: any[];
+  brandProducts: any;
   productValue: productdata[] = [];
 
   constructor(
@@ -37,11 +38,22 @@ export class AddDistributorComponent implements OnInit {
     private apiService: ApiService,
     public _brands: BrandService
   ) {
+    
     this.seletedBrand = this._brands.getBrand();
-    this.apiService.readAll(`products?brandId=${this.seletedBrand._id}`).subscribe(data =>{
+    this.route.queryParams.subscribe(params => {
+      this.id = params.id;
+      if(this.id) {
+        this.loadDistributorDetails();
+      }
+    })
+
+    this.apiService.readAll(`products?brandId=${this.seletedBrand._id}`).subscribe(data =>{ 
       this.brandProducts = data.products;
-      for(let product of this.brandProducts){
-        this.productValue.push({productId:product._id, price:"0"})
+      console.log(data)
+      if(!this.id){
+        for(let product of this.brandProducts){
+          this.productValue.push({productId:product._id, price:"0"})
+        }
       }
     })
     this.apiService.readAll("route").subscribe(data => this.routeList = data.routes )
@@ -49,6 +61,33 @@ export class AddDistributorComponent implements OnInit {
 
   onChangeRoute(value:any){
     this.DropPoint = this.routeList.filter(item => item._id === value)[0]["locations"];
+  }
+
+  loadDistributorDetails() {
+    this.apiService
+      .readSingle("distributor", this.id)
+      .pipe(first())
+      .subscribe(
+        (data) => {
+          this.f.name.setValue(data.distributor.name);
+          this.f.route.setValue(data.distributor.route._id);
+          this.f.email.setValue(data.distributor.email);
+          this.f.phone.setValue(data.distributor.phone);
+          this.f.address.setValue(data.distributor.address);
+          this.f.crateLimit.setValue(data.distributor.crateLimit);
+          this.f.cashLimit.setValue(data.distributor.cashLimit);
+          this.formType = `Edit ${data.distributor.name}`;
+          this.brandProducts = [];
+          this.brandProducts = data.distributor.products;
+          console.log(this.brandProducts)
+          
+        },
+        (error) => {
+          console.log(error)
+          this.alertService.error(error);
+          this.loading = false;
+        }
+      );
   }
 
   ngOnInit() {
@@ -69,7 +108,6 @@ export class AddDistributorComponent implements OnInit {
 
   onChangeEvent(event: any, productId: any){
     this.productValue.filter(item => item.productId === productId)[0].price = event.target.value;
-    console.log(this.productValue)
   }
 
   // convenience getter for easy access to form fields
@@ -79,7 +117,6 @@ export class AddDistributorComponent implements OnInit {
 
   onSubmit() {
     this.submitted = true;
-
     // reset alerts on submit
     this.alertService.clear();
 
@@ -91,8 +128,25 @@ export class AddDistributorComponent implements OnInit {
 
     this.form.value.brand = this.seletedBrand._id;
     this.form.value.products = this.productValue;
-    this.apiService
-      .create("distributor", this.form.value)
+    console.log(this.form.value)
+
+    if(!this.id) {
+      this.apiService
+        .create("distributor", this.form.value)
+        .pipe(first())
+        .subscribe(
+          (data) => {
+            this.router.navigate(['distributors']);
+          },
+          (error) => {
+            console.log(error)
+            this.alertService.error(error);
+            this.loading = false;
+          }
+        );
+    }else{
+      this.apiService
+      .update("distributor", this.form.value, this.id)
       .pipe(first())
       .subscribe(
         (data) => {
@@ -104,5 +158,6 @@ export class AddDistributorComponent implements OnInit {
           this.loading = false;
         }
       );
+    }
   }
 }
