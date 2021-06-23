bc5f9a20d4f63b564da1085cd975f1c5ecb7b58d
root <root@ip-172-31-37-123.ap-south-1.compute.internal>
2021-06-23 06:08:01 +0000

    port change in api

--- Api/server.js
+++ Api/server.js
@@ -91,7 +91,7 @@ require("./app/routes/delveryboy.routes")(app);
 
 app.use(apiErrorHandler);
 // set port, listen for requests
-const PORT = process.env.PORT || 8087;
+const PORT = process.env.PORT || 8088;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
